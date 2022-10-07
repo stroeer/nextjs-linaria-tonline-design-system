2eec388b74abf5aa89e0a797d447b5ddcd4086d2
Lukas Bombach <lukas.bombach@googlemail.com>
2022-10-07 16:07:59 +0200

    text styles wip

---
+++ design/text/body.ts
@@ -0,0 +1,19 @@
+import { css } from "@linaria/core";
+
+export const large = css`
+  font-size: var(--font-size-18);
+  line-height: var(--line-height-17);
+  color: var(--color-trout);
+`;
+
+export const regular = css`
+  font-size: var(--font-size-14);
+  line-height: var(--line-height-15);
+  color: var(--color-trout);
+`;
+
+export const small = css`
+  font-size: var(--font-size-12);
+  line-height: var(--line-height-15);
+  color: var(--color-trout);
+`;
