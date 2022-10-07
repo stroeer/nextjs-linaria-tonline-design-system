5fa1f14062e8bf85158e31816f51f00b24e81a62
Lukas Bombach <lukas.bombach@googlemail.com>
2022-10-07 17:06:29 +0200

    wip todos

---
+++ design/text/teaser.ts
@@ -0,0 +1,17 @@
+import { css } from "@linaria/core";
+
+export const label = css`
+  /* todo */
+`;
+
+export const labelSmall = css`
+  /* todo */
+`;
+
+export const kicker = css`
+  /* todo */
+`;
+
+export const footer = css`
+  /* todo */
+`;
