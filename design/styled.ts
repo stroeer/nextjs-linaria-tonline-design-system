568d838bb64a6765461f1537784b717ed20f15ea
Lukas Bombach <lukas.bombach@googlemail.com>
2022-10-08 17:37:50 +0200

    typing the styled fn

--- design/styled.ts
+++ design/styled.ts
@@ -1,6 +1,11 @@
 import { createElement } from "react";
 import { cx } from "@linaria/core";
 
-export function styled(tag: string, ...classNames: string[]) {
+import type { FC, PropsWithChildren } from "react";
+
+export function styled<Tag extends keyof JSX.IntrinsicElements>(
+  tag: Tag,
+  ...classNames: string[]
+): FC<PropsWithChildren<JSX.IntrinsicElements[Tag]>> {
   return ({ children }) => createElement(tag, { className: cx(...classNames) }, children);
 }
