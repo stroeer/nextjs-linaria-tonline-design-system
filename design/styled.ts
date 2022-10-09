3c2827103ea04e848f3b8f39bb84a97b22836555
Lukas Bombach <lukas.bombach@googlemail.com>
2022-10-09 12:20:38 +0200

    Update styled.ts

--- design/styled.ts
+++ design/styled.ts
@@ -6,8 +6,6 @@ import type { FC, PropsWithChildren } from "react";
 type Tag = keyof JSX.IntrinsicElements;
 type StyledComponent<T extends Tag> = FC<PropsWithChildren<JSX.IntrinsicElements[T]>>;
 
-type A = JSX.IntrinsicElements["a"];
-
 export function styled<T extends Tag>(tag: T, ...classNames: string[]): StyledComponent<T> {
   const component: StyledComponent<T> = ({ children, ...props }) =>
     createElement(tag, { className: cx(...classNames), ...props }, children);
